bacda9c27c00459b291c3ca465380a38ab7b531f
Elena Pavlova <e.pavlova@bi.zone>
2019-09-06 14:21:30 +0300

    Создан новый проект. + тест на проверку авторизации

---
+++ fixture/application.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from fixture.session import sessionHelper
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome('C:\\Users\\e.pavlova\\Desktop\\tmp\\chromedriver_win32\\chromedriver.exe')
+            #self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd == webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        #self.wd.implicitly_wait(5)
+        self.session = sessionHelper(self)
+        self.base_url = base_url
+
+    def open_home_page(self):
+        wd = self.wd
+        #wd.get("http://localhost:8080/addressbook/")
+        wd.get(self.base_url)
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def destroy(self):
+        self.wd.quit()
+
+
